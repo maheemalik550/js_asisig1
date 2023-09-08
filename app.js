44e9d1a199361fedcb8b545996cf9d236fe9787c
maheemalik550 <maheemalik550@gmail.com>
2023-09-08 23:13:04 +0500

    complete work

---
+++ app.js
@@ -0,0 +1,82 @@
+// chapter 1 Alert 
+// Q1
+// alert("Maheen");
+// alert("Mushtaq");
+// alert("maheenmalik550@gmail.com");
+// alert("03201289759");
+// alert("123456789");
+// Q2
+// alert("you're learning javascript");
+// Q3 
+// alert("Hellow World");
+// chapter 2 Variables for String 
+// Q1
+// var firstName = "maheen";
+// console.log(firstName);
+// Q2
+// var newWork;
+// newWork = "assignment";
+// console.log(newWork);
+// Q 3
+// var teamName = "ftmgroup";
+// console.log(teamName);
+// Q 4
+// var bestMan = "Charlie";
+// console.log(bestMan);
+// chapter 3 Variable For Numbers 
+// 1)
+// var caseQty;
+// 2)
+// var caseQty = 144;
+// console.log(caseQty);
+// 3)
+// var numb = 9;
+// console.log(numb);
+// 4)
+// var num = 5;
+// num + 6;
+// console.log(num + 6);
+// 5) 
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+// Q6
+// var num1 = 3;
+// var num2 = num1 + 3;
+// console.log(num2);
+// Chapter 4(variable names legal and illegal)
+// Q 1
+// var productCost = 3.45;
+// Q 2
+// var nameOfband;
+// Q3
+// var firstNum = 3;
+// Q4
+// var firstName = "maheen";
+// var lastName = "malik"
+// var fullName = firstName + lastName;
+// console.log(fullName);
+// Q 5
+// legal(fullName , full_name , smart3work , &price)
+// illegal(123work , new line , @name ) 
+// chapter 5 mathematical expression
+// Q1) /
+// Q 2
+// var num1 = 20;
+// var num2 = 6;
+// var res = num1 %  num2;
+// console.log(res)
+// ans is 2
+// Q 3
+// var largeNum = 1000 * 2000;
+// console.log(largeNum);
+// Q 4
+// var num = 4;
+// var res = num - 3 ;
+// console.log(res);
+// Q5 
+// var res = 50 / 10;
+// console.log(res);
+// Q6
+// alert(3 * 6);
